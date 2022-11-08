037ece0c088d688e4be2e2b7ebcf0116210bf8cc
SohailLuffy <sohailislammail97@gmail.com>
2022-11-08 21:48:15 +0530

    MethodOverloading

---
+++ MethodOverloading/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MethodOverload
+{
+
+    class Program
+    {
+
+        void display(int a, string b)
+        {
+            Console.WriteLine("int: " + a);
+            Console.WriteLine("string: " + b);
+        }
+
+        void display(string b, int a)
+        {
+            Console.WriteLine("string: " + b);
+            Console.WriteLine("int: " + a);
+        }
+        static void Main(string[] args)
+        {
+
+            Program p1 = new Program();
+            p1.display(100, "Programming");
+            p1.display("Programiz", 400);
+            Console.ReadLine();
+        }
+    }
+}
